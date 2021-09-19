e4968605ab322d29db2a786a9a3460a359e4e151
jake <jakebs540@gmail.com>
2021-09-19 14:18:12 +0100

    Tests working with database

--- lib/peeps.rb
+++ lib/peeps.rb
@@ -1,8 +1,11 @@
+require 'pg'
+
 class Peeps
   def self.all
-    [
-      "My first peep",
-      "My second peep"
-     ]
+    connection = PG.connect(dbname: 'chitter')
+    result = connection.exec("SELECT * FROM chitter;")
+    result.map do |peep|
+      peep['text']
+    end
   end
 end
